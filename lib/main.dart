3595454881dfc9f4f410c2bbf488abbf5c9d3abb
filipj169 <jamrichfilip16@gmail.com>
2023-12-20 20:16:52 +0100

    i can see the file name

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_sharing_intent/flutter_sharing_intent.dart';
+import 'package:flutter_sharing_intent/model/sharing_file.dart';
+
+
+/*
 void main() {
   runApp(const MyApp());
 }
@@ -29,6 +35,42 @@ class BottomSheetModal extends StatefulWidget {
 }
 
 class _BottomSheetModalState extends State<BottomSheetModal> {
+  late StreamSubscription _intentDataStreamSubscription;
+  List<SharedFile>? list;
+
+  @override
+  void initState() {
+    initSharingListener();
+    super.initState();
+  }
+
+  initSharingListener() {
+    // For sharing images coming from outside the app while the app is in the memory
+    _intentDataStreamSubscription = FlutterSharingIntent.instance
+        .getMediaStream()
+        .listen((List<SharedFile> value) {
+      setState(() {
+        list = value;
+        print(list);
+      });
+      //print("Shared: getMediaStream ${value.map((f) => f.value).join(",")}");
+    }, onError: (err) {
+      print("Shared: getIntentDataStream error: $err");
+    });
+
+    // For sharing images coming from outside the app while the app is closed
+    FlutterSharingIntent.instance
+        .getInitialSharing()
+        .then((List<SharedFile> value) {
+      //print(
+       //   "Shared: getInitialMedia => ${value.map((f) => f.value).join(",")}");
+      setState(() {
+        list = value;
+        print(list);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -181,7 +223,7 @@ class _BottomSheetModalState extends State<BottomSheetModal> {
 
   Widget _buildFileSection() {
     // Tu bude názov zdieľaného súboru
-    return const Column(children: [
+    return Column(children: [
       Align(
         alignment: Alignment.centerLeft,
         child: TextField(
@@ -190,6 +232,83 @@ class _BottomSheetModalState extends State<BottomSheetModal> {
           ),
         ),
       ),
+    Text('Sharing data: \n${list?.join("\n\n")}\n')),
     ]);
+  }
+
+  @override
+  void dispose() {
+    _intentDataStreamSubscription.cancel();
+    super.dispose();
+  }
+}
+
+ */
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late StreamSubscription _intentDataStreamSubscription;
+  List<SharedFile>? list;
+
+  @override
+  void initState() {
+    initSharingListener();
+
+    super.initState();
+  }
+
+  initSharingListener()
+  {
+    // For sharing images coming from outside the app while the app is in the memory
+    _intentDataStreamSubscription = FlutterSharingIntent.instance.getMediaStream()
+        .listen((List<SharedFile> value) {
+      setState(() {
+        list = value;
+      });
+      print("Print:----------> $list");
+      print("Shared: getMediaStream ${value.map((f) => f.value).join(",")}");
+    }, onError: (err) {
+      print("Shared: getIntentDataStream error: $err");
+    });
+
+    // For sharing images coming from outside the app while the app is closed
+    FlutterSharingIntent.instance.getInitialSharing().then((List<SharedFile> value) {
+      print("Shared: getInitialMedia => ${value.map((f) => f.value).join(",")}");
+      setState(() {
+        list = value;
+      });
+      print("Print:----------> $list");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              child: Text('Sharing data: \n${list?.join("\n\n")}\n')),
+        ),
+      ),
+    );
+  }
+  @override
+  void dispose() {
+    _intentDataStreamSubscription.cancel();
+    super.dispose();
   }
 }
